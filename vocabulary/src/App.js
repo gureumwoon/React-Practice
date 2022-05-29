7c669b64cec9359a3ed44483fedbdaeca7ef6701
gureumwoon <vfrvfr3221@gmail.com>
2022-05-29 23:39:09 +0900

    배경 이미지 추가

--- vocabulary/src/App.js
+++ vocabulary/src/App.js
@@ -1,17 +1,26 @@
 // import React from 'react';
 import './App.css';
-import backgroundImg from './assets/bg2.jpg';
+import backgroundImg from './assets/bg7.jpg';
 import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Header>
         <h1 style={{ margin: 0 }}>중국어 단어장</h1>
       </Header>
       <Section>
         <CardContainer>
           <CardItem>
+            <div>
+              <h4>안녕</h4>
+              <p>어쩌구</p>
+            </div>
+            <div>
+            </div>
+            <p></p>
+            <p></p>
+            <p></p>
           </CardItem>
           <CardItem>
           </CardItem>
@@ -20,37 +29,45 @@ function App() {
         </CardContainer>
       </Section>
 
-    </div>
+    </Container>
   );
 }
 
 const CardContainer = styled.div`
   width: 70%;
   display: grid;
-  margin: 0px auto;
+  margin: 0 auto;
   column-gap: 20px;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 200px 200px;
 `
 
+const Section = styled.section`
+  padding-top: 120px;
+`
+
 const CardItem = styled.article`
-  border: 1px solid gray;
+  border: 2px solid gray;
   border-radius: 15px;
 `
 
 const Header = styled.header`
+  position: fixed;
   width: 100%;
-  height: 50px;
-  background-color: #F3EAE1;
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+  background-color: #DBDBDB;
   top: 0;
+  left: 0;
 `
 
-const Section = styled.section`
+const Container = styled.div`
   height: 100vh;
   background-image: url(${backgroundImg});
   background-repeat: no-repeat;
   background-size: cover;
-  padding: 50px 0;
+  background-position:center;
 `
 
 export default App;
